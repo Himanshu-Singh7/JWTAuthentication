a5e04620bc016b267a7d9e1df324c3b57eaacb64
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-22 01:15:09 +0530

    Add files via upload

---
+++ JwtExample3/JwtExample3/src/main/java/com/jwt/exaple/Model/User.java
@@ -0,0 +1,18 @@
+package com.jwt.exaple.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private String userId;
+
+    private String name;
+
+    private String email;
+
+}
