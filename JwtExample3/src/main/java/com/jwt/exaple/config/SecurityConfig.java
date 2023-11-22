33974a7550de8eb9e7ecb277dc1a30f2d2c4e11d
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-23 02:50:21 +0530

    Update SecurityConfig.java

--- JwtExample3/src/main/java/com/jwt/exaple/config/SecurityConfig.java
+++ JwtExample3/src/main/java/com/jwt/exaple/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import java.net.http.HttpClient;
 
 
