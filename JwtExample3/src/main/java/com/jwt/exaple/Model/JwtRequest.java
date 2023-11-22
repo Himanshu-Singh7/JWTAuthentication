adb9127228b703c91e2121dff0ed8a9c903f3c36
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-23 02:47:12 +0530

    Add files via upload
    JWT Authentication with Spring Boot 3.0

---
+++ JwtExample3/src/main/java/com/jwt/exaple/Model/JwtRequest.java
@@ -0,0 +1,14 @@
+package com.jwt.exaple.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtRequest {
+    private String email;
+    private String password;
+}
---
+++ JwtExample3/src/main/java/com/jwt/exaple/Model/JwtResponse.java
@@ -0,0 +1,16 @@
+package com.jwt.exaple.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+
+    private String jwtToken;
+
+    private String username;
+}
